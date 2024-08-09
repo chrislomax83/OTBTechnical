2b75c8e615d69c9fd27c7277fa5a118072c96f1c
christopherlomax <chris@kiscms.co.uk>
2024-08-09 15:45:40 +0100

    feat: adding hotel data loader tests

---
+++ tests/OTBTechnical.Tests/Data/HotelDataLoaderTests.cs
@@ -0,0 +1,14 @@
+using OTBTechnical.Data;
+
+namespace OTBTechnical.Tests.Data;
+
+public class HotelDataLoaderTests
+{
+    
+    [Fact]
+    public async Task Should_Load_Hotel_Data()
+    {
+
+    }
+    
+}
