9312617f27784c48dec20060f4f821bdb8181c7e
christopherlomax <chris@kiscms.co.uk>
2024-08-12 15:04:25 +0100

    refactor: adding data checks with exceptions

--- src/OTBTechnical/Data/AbstractDataLoader.cs
+++ src/OTBTechnical/Data/AbstractDataLoader.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using OTBTechnical.Data.Exceptions;
 using OTBTechnical.Data.Models;
 
 namespace OTBTechnical.Data;
@@ -11,11 +12,23 @@ public abstract class AbstractDataLoader<T> where T : IDataModel
 
     protected async Task LoadData()
     {
+        if (string.IsNullOrEmpty(FileName))
+            throw new DataFileNotFoundException("File name not set. Set in derived class or call SetFileName");
+        
+        if (!File.Exists(FileName)) throw new DataFileNotFoundException($"Data file {FileName} not found");
+        
         using StreamReader fileReader = new(FileName);
 
         var fileContents = await fileReader.ReadToEndAsync();
 
-        Data = JsonSerializer.Deserialize<IReadOnlyList<T>>(fileContents) ?? [];
+        try
+        {
+            Data = JsonSerializer.Deserialize<IReadOnlyList<T>>(fileContents) ?? [];
+        }
+        catch (Exception ex)
+        {
+            throw new DataFileDeserializationException($"Data file {FileName} could not be deserialized", ex);
+        }
     }
 
     public void SetFileName(string fileName)
