cd659321e4a71a241fd1a32fddee3e8543c2a3a1
christopherlomax <chris@kiscms.co.uk>
2024-08-12 14:42:55 +0100

    test: additional flight data tests

--- tests/OTBTechnical.Tests/Features/FlightSearch/FlightDataFilterTests.cs
+++ tests/OTBTechnical.Tests/Features/FlightSearch/FlightDataFilterTests.cs
@@ -23,4 +23,46 @@ public class FlightDataFilterTests
 
         Assert.True(results.Count > 0);
     }
+    
+    [Theory]
+    [InlineData("MAN", "TFS", "2023-07-01", 1, 1)]
+    [InlineData("MAN", "AGP", "2023-07-01", 2, 1)]
+    [InlineData("LGW", "PMI", "2023-06-15", 6 ,1)]
+    [InlineData("LGW", "AGP", "2023-07-01", 11 ,2)]
+    public async Task Should_Return_Filtered_List_With_Cheapest_Check_From_Flight_Search_Request(
+        string departureAirport, string arrivalAirport, string departureDate, int expectedFlightId, int expectedElements)
+    {
+        var flightRequest = new FlightSearchRequest(departureAirport, arrivalAirport, departureDate);
+        var flightDataLoader = new FlightDataLoader();
+        var flightData = await flightDataLoader.GetData();
+        
+        Assert.True(flightData.Count > 0);
+
+        var flightFilter = new FlightDataFilter(flightRequest, flightData);
+        var results = flightFilter.GetResults();
+
+        Assert.Equal(expectedElements, results.Count);
+
+        var firstElement = results.OrderBy(e=>e.Price).First();
+        
+        Assert.Equal(expectedFlightId, firstElement.Id);
+    }
+    
+    [Theory]
+    [InlineData("MAN", "PMI", "2023-07-01")]
+    [InlineData("LGW", "PMI", "2023-07-01")]
+    public async Task Should_Return_No_Elements_From_Flight_Search_Request(
+        string departureAirport, string arrivalAirport, string departureDate)
+    {
+        var flightRequest = new FlightSearchRequest(departureAirport, arrivalAirport, departureDate);
+        var flightDataLoader = new FlightDataLoader();
+        var flightData = await flightDataLoader.GetData();
+        
+        Assert.True(flightData.Count > 0);
+
+        var flightFilter = new FlightDataFilter(flightRequest, flightData);
+        var results = flightFilter.GetResults();
+
+        Assert.Empty(results);
+    }
 }
