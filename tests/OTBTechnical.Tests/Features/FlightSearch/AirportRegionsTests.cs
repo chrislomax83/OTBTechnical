69f820a23e0f6222fa1f3c18447d1adddc67f046
christopherlomax <chris@kiscms.co.uk>
2024-08-12 12:39:13 +0100

    test: airport regions tests

--- tests/OTBTechnical.Tests/Features/FlightSearch/AirportRegionsTests.cs
+++ tests/OTBTechnical.Tests/Features/FlightSearch/AirportRegionsTests.cs
@@ -1,6 +1,29 @@
+using OTBTechnical.Features.FlightSearch;
+
 namespace OTBTechnical.Tests.Features.FlightSearch;
 
 public class AirportRegionsTests
 {
-    
+    [Theory]
+    [InlineData("MAN", "NorthWest")]
+    [InlineData("LGW", "London")]
+    [InlineData("LTN", "London")]
+    public void Should_Return_Airport_Region_From_Code(string airportCode, string expectedRegion)
+    {
+        var airportRegion = AirportRegions.AirportRegionByCode(airportCode);
+        
+        Assert.Equal(expectedRegion, airportRegion);
+    }
+
+    [Theory]
+    [InlineData("London", new string[] {"LGW", "LTN"})]
+    [InlineData("NorthWest", new string[] {"MAN"})]
+    public void Should_Return_AirportCodes_From_Region(string airportRegion, string[] expectedAirportCodes)
+    {
+        var airportCodes = AirportRegions.AirportCodesByRegion(airportRegion);
+
+        airportCodes = airportCodes.OrderBy(e => e).ToArray();
+        
+        Assert.Equal(expectedAirportCodes, airportCodes);
+    }
 }
