73ea69c489639a319819d292d86cf5eeb467d125
christopherlomax <chris@kiscms.co.uk>
2024-08-12 14:58:35 +0100

    test: additional holiday search engine tests

--- tests/OTBTechnical.Tests/Features/HolidaySearch/HolidaySearchEngineTests.cs
+++ tests/OTBTechnical.Tests/Features/HolidaySearch/HolidaySearchEngineTests.cs
@@ -22,4 +22,24 @@ public class HolidaySearchEngineTests
         
         Assert.True(results.Count > 0);
     }
+    
+    [Theory]
+    [InlineData("MAN", "AGP", "2024/07/01", 7)]
+    [InlineData("London", "PMI", "2024/06/15", 10)]
+    [InlineData("Any", "LPA", "2024/11/10", 14)]
+    public async Task Should_Return_No_Elements_For_Holiday_Search_Request(
+        string departingFrom, string travellingTo, string departureDate, int holidayDuration)
+    {
+        var flightSearchEngine = new FlightSearchEngine();
+
+        var hotelSearchEngine = new HotelSearchEngine();
+
+        var holidaySearchEngine = new HolidaySearchEngine(flightSearchEngine, hotelSearchEngine);
+
+        var holidaySearchRequest = new HolidaySearchRequest(departingFrom, travellingTo, departureDate, holidayDuration);
+
+        var results = await holidaySearchEngine.Search(holidaySearchRequest);
+        
+        Assert.Empty(results);
+    }
 }
