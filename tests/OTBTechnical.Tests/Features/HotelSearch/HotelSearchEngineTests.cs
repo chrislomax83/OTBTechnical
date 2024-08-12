6a22dcd7a071d154c12aa96d532af39d18ea2177
christopherlomax <chris@kiscms.co.uk>
2024-08-12 11:38:37 +0100

    test: hotel search engine test

--- tests/OTBTechnical.Tests/Features/HotelSearch/HotelSearchEngineTests.cs
+++ tests/OTBTechnical.Tests/Features/HotelSearch/HotelSearchEngineTests.cs
@@ -1,8 +1,25 @@
+using OTBTechnical.Features.HotelSearch;
+using OTBTechnical.Features.HotelSearch.Models.Requests;
+
 namespace OTBTechnical.Tests.Features.HotelSearch;
 
 public class HotelSearchEngineTests
 {
     
-    public void Should_Return_All_Hotels_For_Search_Term() {}
+    [Theory]
+    [InlineData(7, "2022-11-05", "TFS", 2)]
+    [InlineData(14, "2023-06-15", "PMI", 2)]
+    [InlineData(14, "2022-11-10", "LPA", 1)]
+    public async Task Should_Return_All_Hotels_For_Search_Term(
+        int noOfNights, string arrivalDate, string destinationAirport, int expectedElements)
+    {
+        var hotelRequest = new HotelSearchRequest(noOfNights, arrivalDate, destinationAirport);
+
+        var searchEngine = new HotelSearchEngine();
+        
+        var searchResults = await searchEngine.Search(hotelRequest);
+        
+        Assert.Equal(expectedElements, searchResults.Count);
+    }
     
 }
