5de91dd3cd4a21d90af96962a083945059cf893d
christopherlomax <chris@kiscms.co.uk>
2024-08-12 11:08:58 +0100

    test: hotel data filter test

--- tests/OTBTechnical.Tests/Features/HotelSearch/HotelDataFilterTests.cs
+++ tests/OTBTechnical.Tests/Features/HotelSearch/HotelDataFilterTests.cs
@@ -1,6 +1,26 @@
+using OTBTechnical.Data;
+using OTBTechnical.Features.HotelSearch;
+using OTBTechnical.Features.HotelSearch.Models.Requests;
+
 namespace OTBTechnical.Tests.Features.HotelSearch;
 
 public class HotelDataFilterTests
 {
-    public void Should_Return_Filtered_List_With_Any_Elements_From_Hotel_Search_Request() {}
+    [Theory]
+    [InlineData(7, "2022-11-05", "TFS")]
+    [InlineData(14, "2023-06-15", "PMI")]
+    public async Task Should_Return_Filtered_List_With_Any_Elements_From_Hotel_Search_Request(
+        int noOfNights, string arrivalDate, string destinationAirport)
+    {
+        var hotelRequest = new HotelSearchRequest(noOfNights, arrivalDate, destinationAirport);
+        var hotelDataLoader = new HotelDataLoader();
+        var hotelData = await hotelDataLoader.GetData();
+        
+        Assert.True(hotelData.Count > 0);
+
+        var hotelFilter = new HotelDataFilter(hotelRequest, hotelData);
+        var results = hotelFilter.GetResults();
+
+        Assert.True(results.Count > 0);
+    }
 }
