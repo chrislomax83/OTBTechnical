16d0cb0d08fab4a40ca44cca079627db43e563f6
christopherlomax <chris@kiscms.co.uk>
2024-08-12 10:24:23 +0100

    test: adding empty hotel data filter tests

---
+++ tests/OTBTechnical.Tests/Features/HotelSearch/HotelDataFilterTests.cs
@@ -0,0 +1,6 @@
+namespace OTBTechnical.Tests.Features.HotelSearch;
+
+public class HotelDataFilterTests
+{
+    public void Should_Return_Filtered_List_With_Any_Elements_From_Hotel_Search_Request() {}
+}
