d8302efefa997709651053297f1fbe2bad58de46
christopherlomax <chris@kiscms.co.uk>
2024-08-12 13:50:38 +0100

    test: search tests for request validation

--- tests/OTBTechnical.Tests/Features/FlightSearch/FlightSearchEngineTests.cs
+++ tests/OTBTechnical.Tests/Features/FlightSearch/FlightSearchEngineTests.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using OTBTechnical.Features.FlightSearch;
 using OTBTechnical.Features.FlightSearch.Models.Requests;
 
@@ -20,5 +21,19 @@ public class FlightSearchEngineTests
         var searchResults = await searchEngine.Search(flightRequest);
         
         Assert.Equal(expectedElements, searchResults.Count);
+    }
+    
+    [Theory]
+    [InlineData("", "TFS", "2023-07-01")]
+    [InlineData("MAN", "", "2023-07-01")]
+    [InlineData("MAN", "TFS", "2023-13-01")]
+    public async Task Should_Throw_ValidationException_For_Invalid_Search_Term_Arguments(
+        string departureAirport, string arrivalAirport, string departureDate)
+    {
+        var flightRequest = new FlightSearchRequest(departureAirport, arrivalAirport, departureDate);
+
+        var searchEngine = new FlightSearchEngine();
+        
+        await Assert.ThrowsAsync<ValidationException>(() => searchEngine.Search(flightRequest));
     }
 }
--- tests/OTBTechnical.Tests/Features/HotelSearch/HotelSearchEngineTests.cs
+++ tests/OTBTechnical.Tests/Features/HotelSearch/HotelSearchEngineTests.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using OTBTechnical.Features.HotelSearch;
 using OTBTechnical.Features.HotelSearch.Models.Requests;
 
@@ -20,6 +21,20 @@ public class HotelSearchEngineTests
         var searchResults = await searchEngine.Search(hotelRequest);
         
         Assert.Equal(expectedElements, searchResults.Count);
+    }
+    
+    [Theory]
+    [InlineData(0, "2024-11-05", "TFS")]
+    [InlineData(6, "2024-13-15", "PMI")]
+    [InlineData(3, "2024-11-10", "LPA3")]
+    public async Task Should_Throw_ValidationException_For_Invalid_Search_Term_Arguments(
+        int noOfNights, string arrivalDate, string destinationAirport)
+    {
+        var hotelRequest = new HotelSearchRequest(noOfNights, arrivalDate, destinationAirport);
+
+        var searchEngine = new HotelSearchEngine();
+        
+        await Assert.ThrowsAsync<ValidationException>(() => searchEngine.Search(hotelRequest));
     }
     
 }
